1c1064410dac7c1351d03495774a463c6505ee66
Nihal <nihalpnazer@gmail.com>
2023-05-18 23:37:18 +0530

    ui changes

--- Main_Project/My_Project/CrowdCount/CrowdCount/urls.py
+++ Main_Project/My_Project/CrowdCount/CrowdCount/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.upload, name='upload'),  # Add this line
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
